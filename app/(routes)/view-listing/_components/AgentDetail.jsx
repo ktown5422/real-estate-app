5211007364a16b3cce36ce459af47630c7a043fd
Kevin Townson <ktown5422@gmail.com>
2024-08-26 15:02:15 -0500

    update - KT

--- app/(routes)/view-listing/_components/AgentDetail.jsx
+++ app/(routes)/view-listing/_components/AgentDetail.jsx
@@ -7,12 +7,8 @@ function AgentDetail({ listingDetail }) {
     <div className='flex gap-5 items-center justify-between 
     p-5 rounded-lg shadow-md border my-6'>
       <div className='flex items-center gap-6'>
-        <Image src={listingDetail?.profileImage}
-          alt='profileImage'
-          width={60}
-          height={60}
-          className='rounded-full'
-        />
+        <Image src={'/logo.svg'} width={150}
+          height={0} alt='logo' />
         <div>
           {/* <h2 className='text-lg font-bold'>{listingDetail?.fullName}</h2> */}
           <h2 className='text-gray-500'>{listingDetail?.createdBy}</h2>
--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
     <div className='p-6 px-10 flex justify-between shadow-sm fixed top-0 w-full z-10 bg-white'>
       <div className='flex gap-12 items-center'>
         <Link href={'/'}>
-          <Image src={''} width={150}
+          <Image src={'/logo.svg'} width={150}
             height={0} alt='logo' />
         </Link>
         <ul className='hidden md:flex gap-10'>
